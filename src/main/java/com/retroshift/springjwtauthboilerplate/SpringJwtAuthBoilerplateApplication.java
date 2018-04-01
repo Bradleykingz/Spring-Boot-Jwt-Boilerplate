2870d8f6e5a8540e5b6cfa5b3a89c59c627ff744
Bradley Kingsley <bradstarart@gmail.com>
2018-04-01 18:29:14 +0300

    Initial Commit

---
+++ src/main/java/com/retroshift/springjwtauthboilerplate/SpringJwtAuthBoilerplateApplication.java
@@ -0,0 +1,12 @@
+package com.retroshift.springjwtauthboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAuthBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtAuthBoilerplateApplication.class, args);
+	}
+}
